e14db2ba0ad221a3db55cdf6e8df2b9a953fffae
elahaye <e.lahaye49@gmail.com>
2020-07-30 14:03:15 +0200

    CRUD Admin Categories

--- application/controllers/CategoryController.php
+++ application/controllers/CategoryController.php
@@ -5,12 +5,23 @@ class CategoryController extends Controller
     public $showCategories;
     public $categoryModel;
     public $categoryId;
+    public $name;
 
     public function __construct()
     {
         $this->categoryModel = new CategoryModel();
 
-        $this->displayCategories();
+        if ($_SERVER['REQUEST_METHOD'] == "POST" && isset($_GET['editId'])) {
+            if ($_GET['editId'] != '') {
+                $this->editCategory();
+            } else {
+                $this->addCategory();
+            }
+        } elseif (isset($_GET['id'])) {
+            $this->editForm();
+        } else {
+            $this->displayCategories();
+        }
     }
 
     /**
@@ -24,6 +35,45 @@ class CategoryController extends Controller
         $this->showCategories = $query->fetchAll();
     }
 
+    public function addCategory()
+    {
+        $this->name = $_POST['nameCategory'];
+
+        $this->categoryModel->addCategory($this->name);
+
+        // Redirect to the articles list page
+        Router::redirectTo('listCategories');
+        exit();
+    }
+
+    /* Display in the edit form the category who's going to be edited
+     * 
+     * @return void
+     */
+    public function editForm(): void
+    {
+        $this->categoryId = $_GET['id'];
+
+        $editCategory = $this->categoryModel->displayOneCategory($this->categoryId);
+
+        $this->name = $editCategory['name'];
+    }
+
+    public function editCategory()
+    {
+        $this->categoryId = $_GET['editId'];
+        $this->name = $_POST['nameCategory'];
+
+        $this->categoryModel->editCategory($this->categoryId, $this->name);
+
+        // Redirect to the articles list page
+        Router::redirectTo('listCategories');
+        exit();
+    }
+
+    /**
+     * Delete a catégory
+     */
     public function deleteCategory()
     {
         $this->categoryId = $_GET['deleteId'];
--- application/models/CategoryModel.php
+++ application/models/CategoryModel.php
@@ -31,6 +31,21 @@ class CategoryModel extends Model
         return $display_category;
     }
 
+    public function addCategory(string $name)
+    {
+        $sql = 'INSERT INTO `categories` 
+                SET name = :name';
+        $add_category = $this->pdo->prepare($sql);
+        $add_category->execute(['name' => $name]);
+    }
+
+    public function editCategory(int $category_id, string $name)
+    {
+        $sql = 'UPDATE `categories` SET name = :name WHERE id = :category_id';
+        $edit_category = $this->pdo->prepare($sql);
+        $edit_category->execute(compact('category_id', 'name'));
+    }
+
     public function deleteCategory(int $category_id): void
     {
         $sql = 'DELETE FROM `categories` WHERE id = :id';
