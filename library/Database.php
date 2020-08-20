8179319f78fc1bda0b1aa04e9d17fce72b804429
elahaye <e.lahaye49@gmail.com>
2020-08-20 19:34:21 +0200

    Database name changed

--- library/Database.php
+++ library/Database.php
@@ -1,24 +1,24 @@
 <?php
 
-class Database 
+class Database
 {
     /**
      * Return a connexion to the database
      * 
      * @return PDO
-     */  
-    public static function connect(): PDO 
+     */
+    public static function connect(): PDO
     {
         $host = "localhost";
-        $dbname = "upgradelife";
+        $dbname = "level_up";
         $user = "root";
         $password = "";
 
-        $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8",$user,$password, [
+        $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $password, [
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
         ]);
-        
+
         return $pdo;
     }
 }
